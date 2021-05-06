0c36c7d222892f6e890f548ffe5dc906b9b6dead
Chip Long <chip_long@yahoo.com>
2021-05-06 11:53:04 -0600

    added in fetch calls to get data from apis on page load assigned to variables

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let jokeList = [];
+let triviaList = [];
+
+// https://jservice.io/
+function getTriviaFromAPI() {
+  // let requestURL = "http://jservice.io/api/clues";
+  let requestURL = "https://opentdb.com/api.php?amount=10&type=multiple";
+  // let requestURL = "http://jservice.io/api/categories?count=100";
+
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+      triviaList = data;
+    });
+}
+
+// https://github.com/15Dkatz/official_joke_api
+function getJokeFromAPI() {
+  let requestURL = "https://official-joke-api.appspot.com/random_ten";
+
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+      jokeList = data;
+    });
+}
+
+$(document).ready(function () {
+  getJokeFromAPI();
+  getTriviaFromAPI();
+});
