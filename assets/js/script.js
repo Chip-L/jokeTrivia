293e8f1b143d39cac934dc7be786688d41af5a17
PurpleAmaranth <36141949+PurpleAmaranth@users.noreply.github.com>
2021-05-07 15:15:38 -0600

    Added Game Over image and formatting.

--- assets/js/script.js
+++ assets/js/script.js
@@ -221,7 +221,7 @@ function getInitials() {
   clearInterval(timerInterval);
   let formContainer = $("<div>").addClass("gameOverForm");
   $("#finalScoreScreen").append(formContainer);
-  $(formContainer).append("<h1>GAME OVER!</h1>");
+  $(formContainer).append("<h1><img class='game-over' src='./assets/images/game-over.png'></h1>");
   let finalScoreHeader = $("<h2>");
   finalScoreHeader.text("Final Score: " + userScore);
   let timeLeft = $("<h2>");
